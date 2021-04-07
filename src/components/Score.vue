85f8c6507bc3475d609935a9d43f94c1c5f36764
tkp14 <tk.091438@gmail.com>
2021-04-07 19:32:14 +0900

    Score.vueのcss以外完成

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,13 +1,31 @@
 <template>
-  
+  <div id="score">
+    <h2>せいせき</h2>
+    <button type="button" @click.prevent="dataReset">クリア</button>
+    <ul v-if="scores">
+      <li v-for="(score, i) of $parent.$data.scores">
+        <span>{{ (i + 1) }}:</span>
+        {{ score.message }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'score',
+  props: ['scores'],
+  data() {
+    return{}
+  },
+  methods: {
+    dataReset() {
+      this.$parent.$data.scores = [];
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
